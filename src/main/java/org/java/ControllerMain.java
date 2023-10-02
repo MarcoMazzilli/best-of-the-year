bf7e7f0d5274ba36f25a69ef19275b90715928d5
MarcoMazzilli <mazzillimarco97@gmail.com>
2023-10-02 16:25:38 +0200

    [Add]Thymeleaf templeate for songs/movies

--- src/main/java/org/java/ControllerMain.java
+++ src/main/java/org/java/ControllerMain.java
@@ -1,5 +1,9 @@
 package org.java;
 
+import java.util.ArrayList;
+
+import org.java.clas.Movie;
+import org.java.clas.Song;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,5 +19,55 @@ public class ControllerMain {
 		model.addAttribute("name", name);
 		
 		return "index";
+	}
+	
+	private ArrayList<Movie> getBestMovies() {
+		
+		ArrayList<Movie> movies = new ArrayList<Movie>();
+		
+		Movie m1 = new Movie("Quasi amici", 1);
+		Movie m2 = new Movie("Interstellar", 2);
+		Movie m3 = new Movie("Trainspotting", 3);
+		
+		movies.add(m1);
+		movies.add(m2);
+		movies.add(m3);
+		
+		
+		return movies;
+	}
+	
+	@GetMapping("/movies")
+	public String getMovies(Model model) {
+		
+		model.addAttribute("movie", getBestMovies());
+		
+		
+		 return "movies";
+	}
+	
+	private ArrayList<Song> getBestSongs() {
+		
+		ArrayList<Song> songs = new ArrayList<Song>();
+		
+		Song s1 = new Song("First date", 1);
+		Song s2 = new Song("Stairway to Heaven", 2);
+		Song s3 = new Song("WallaWalla", 3);
+		
+		songs.add(s1);
+		songs.add(s2);
+		songs.add(s3);
+		
+		
+		return songs;
+	}
+
+	@GetMapping("/songs")
+	public String getSongs(Model model) {
+		
+		model.addAttribute("song", getBestSongs());
+		
+		
+		 return "songs";
 	}
 }
---
+++ src/main/java/org/java/clas/Movie.java
@@ -0,0 +1,37 @@
+package org.java.clas;
+
+public class Movie {
+	
+	private String name;
+	private int id;
+	
+	public Movie(String name,int id) {
+		
+		setName(name);
+		setId(id);
+		
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	@Override
+	public String toString() {
+
+		return getName();
+	}
+
+}
